3ad0800a6617879bc239578b23517a8f13d79baa
Augustseverinandersen <augustseverinandersen@hotmail.com>
2023-05-03 18:33:36 +0200

    code done

--- src/logistic_reg_classifier.py
+++ src/logistic_reg_classifier.py
@@ -4,6 +4,7 @@ import os
 import sys
 sys.path.append(".")
 import argparse
+import zipfile
 
 # Data munging tools
 import pandas as pd
@@ -15,61 +16,65 @@ from sklearn.model_selection import train_test_split, ShuffleSplit
 from sklearn import metrics
 from joblib import dump, load
 
-# Visualisation
+
 
 
 # Defining a function for the user to input a filepath
 def input_parse():
     # initialize the parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("--filepath", type=str) # argument is filepath as a string
+    parser.add_argument("--zip_path", type=str, help = "Path to the zip file")
     args = parser.parse_args()
 
     return args
 
+def unzip(args):
+    folder_path = os.path.join("data", "fake_or_real_news.csv") # Defining the folder_path to the data.
+    if not os.path.exists(folder_path): # If the folder path does not exist, unzip the folder, if it exists do nothing 
+        print("Unzipping file")
+        path_to_zip = args.zip_path # Defining the path to the zip file
+        zip_destination = os.path.join("data") # defining the output destination
+
+        with zipfile.ZipFile(path_to_zip,"r") as zip_ref: # using the package from zipfile, to un zip the zip file
+            zip_ref.extractall(zip_destination) # Unzipping
+    print("The files are unzipped")
+    return folder_path
+
+
 # Loading data
-def loading_data(args): 
+def loading_data(folder_path): 
     print("Loading data")
-    data = pd.read_csv(args.filepath, index_col=0) # loading the data as a pandas dataframe 
+    data = pd.read_csv(folder_path, index_col=0) # loading the data as a pandas dataframe 
     return data
-
-
 
 
 # Assigning data
 def assigning_data(data): 
-    print("assigning data")
+    print("Assigning data")
     X = data["text"] # column text to variable X
     y = data["label"] # column label to variable Y
     return X, y
 
-    
-
 
 # Creating train test split of 80/20
 def split(text, label):
-    print("creating 80/20 split")
+    print("Creating 80/20 split")
     X_train, X_test, y_train, y_test = train_test_split(text,          # texts for the model
                                                         label,          # classification labels
-                                                        test_size=0.2,   # create an 80/20 split
+                                                        test_size=0.2,   # create an 80/20 split for train / test
                                                         random_state=42) # chosing not to get a random split everytime
     return X_train, X_test, y_train, y_test
-
-
-
 
 
 # Vectorizing and Feature Extraction 
 def vectorizer_function():
     print("Vectorizing and feature extraction")
-    vectorizer = CountVectorizer(ngram_range = (1,2),     # unigrams and bigrams (1 word and 2 word units)
-                                lowercase =  True,       # why use lowercase?
+    vectorizer = CountVectorizer(ngram_range = (1,2),    # unigrams and bigrams (1 word and 2 word units)
+                                lowercase =  True,       # Setting everything to lowercase
                                 max_df = 0.95,           # remove very common words
                                 min_df = 0.05,           # remove very rare words
                                 max_features = 100)      # keep only top 100 features
     return vectorizer
-
-
 
 
 # fit_transforming training data 
@@ -106,34 +111,35 @@ def performance(label_test, label_prediction):
 
 # Saving report
 def report_save_function(classifier_metrics):
-    folder_path = os.path.join(".", "out")
-    file_name = "logestic_reg_classifier_metrics.txt"
-    file_path = os.path.join(folder_path, file_name)
+    folder_path = os.path.join(".", "out") # Defining save path
+    file_name = "logestic_reg_classifier_metrics.txt" # Defining file name
+    file_path = os.path.join(folder_path, file_name) # Combining
 
     with open(file_path, "w") as f: # "Writing" the classifier metrics, thereby saving it.
         f.write(classifier_metrics)
     print("Reports saved")
 
 # Saving models
-def model_save_function(classifier, vectorizer):
-    dump(classifier, "models/logestic_reg_LR_classifier.joblib") # Saving the models in folder models as a joblib file.
-    dump(vectorizer, "models/logestic_reg_tfidf_vectorizer.joblib")
+def model_save_function(classifier, vectorizer): # Saving the models in folder models as a joblib file.
+    dump(classifier, "models/logestic_reg_LR_classifier.joblib") # Classifier 
+    dump(vectorizer, "models/logestic_reg_tfidf_vectorizer.joblib") # Feature extraction
     print("Models saved")
 
 
 def main_function():
     print("Logistic Regression Classifier Script:")
-    args = input_parse()
-    data = loading_data(args)
-    X, y = assigning_data(data)
-    X_train, X_test, y_train, y_test = split(X, y)
-    vectorizer = vectorizer_function()
-    X_train_feats, X_test_feats = transform(X_train, X_test, vectorizer)
-    feature_names = feature(vectorizer)
-    classifier, y_pred = classifier_function(X_train_feats, y_train, X_test_feats)
-    classifier_metrics = performance(y_test, y_pred)
-    report_save_function(classifier_metrics)
-    model_save_function(classifier, vectorizer)
+    args = input_parse() # Command line arguments
+    folder_path = unzip(args) # unzip zip file
+    data = loading_data(folder_path) # Reading the data as dataframe
+    X, y = assigning_data(data) # Assigning data
+    X_train, X_test, y_train, y_test = split(X, y) # Splitting data 
+    vectorizer = vectorizer_function() # Parameters for vectorizing 
+    X_train_feats, X_test_feats = transform(X_train, X_test, vectorizer) # Transforming the data 
+    feature_names = feature(vectorizer) # Features 
+    classifier, y_pred = classifier_function(X_train_feats, y_train, X_test_feats) # Classifying 
+    classifier_metrics = performance(y_test, y_pred) # Classification report
+    report_save_function(classifier_metrics) # Saving report
+    model_save_function(classifier, vectorizer) # Saving model
 
-if __name__ == "__main__":
+if __name__ == "__main__": # If script called from terminal run main function
     main_function()
--- src/neural_network.py
+++ src/neural_network.py
@@ -5,6 +5,7 @@ import os
 import sys
 sys.path.append("utils")
 import argparse
+import zipfile
 
 # data munging tools
 import pandas as pd
@@ -20,27 +21,38 @@ from sklearn import metrics
 
 from joblib import dump, load
 
-# Arg parse
+# Defining a function for the user to input a filepath
 def input_parse():
     # initialize the parser
     parser = argparse.ArgumentParser()
-    # add arguments // "--filepath" 
-    parser.add_argument("--filepath", type=str) # argument is filepath as a string
-    # parse the arguments from command line
+    parser.add_argument("--zip_path", type=str, help = "Path to the zip file")
     args = parser.parse_args()
 
     return args
 
+def unzip(args):
+    folder_path = os.path.join("data", "fake_or_real_news.csv") # Defining the folder_path to the data.
+    if not os.path.exists(folder_path): # If the folder path does not exist, unzip the folder, if it exists do nothing 
+        print("Unzipping file")
+        path_to_zip = args.zip_path # Defining the path to the zip file
+        zip_destination = os.path.join("data") # defining the output destination
+
+        with zipfile.ZipFile(path_to_zip,"r") as zip_ref: # using the package from zipfile, to un zip the zip file
+            zip_ref.extractall(zip_destination) # Unzipping
+    print("The files are unzipped")
+    return folder_path
+
 
 # Reading the data 
-def loading_data(args): 
+def loading_data(folder_path): 
     print("Loading data")
-    data = pd.read_csv(args.filepath, index_col=0) # loading the data as a pandas dataframe 
+    data = pd.read_csv(folder_path, index_col=0) # loading the data as a pandas dataframe 
     return data
+
 
 # Assigning the data
 def assigning_data(data): 
-    print("assigning data")
+    print("Assigning data")
     X = data["text"] # column text to variable X
     y = data["label"] # column label to variable Y
     return X, y
@@ -48,22 +60,24 @@ def assigning_data(data):
 
 # Creating train test split of 80/20
 def split(text, label):
-    print("creating 80/20 split")
+    print("Creating 80/20 split")
     X_train, X_test, y_train, y_test = train_test_split(text,          # texts for the model
                                                         label,          # classification labels
-                                                        test_size=0.2,   # create an 80/20 split
+                                                        test_size=0.2,   # create an 80/20 split for train test
                                                         random_state=42) # chosing not to get a random split everytime
     return X_train, X_test, y_train, y_test
+
 
 # Vectorizing and Feature Extraction 
 def vectorizer_function():
     print("Vectorizing and feature extraction")
     vectorizer = CountVectorizer(ngram_range = (1,2),     # unigrams and bigrams (1 word and 2 word units)
-                                lowercase =  True,       # why use lowercase?
+                                lowercase =  True,       # Setting all to lowercase
                                 max_df = 0.95,           # remove very common words
                                 min_df = 0.05,           # remove very rare words
                                 max_features = 500)      # keep only top 500 features
     return vectorizer
+
 
 # fit_transforming training data 
 def transform(text_train, text_test, vectorizer):
@@ -75,19 +89,22 @@ def transform(text_train, text_test, vectorizer):
 
     return X_train_feats, X_test_feats 
 
+
 # get feature names
 def feature(vectorizer):
     feature_names = vectorizer.get_feature_names_out() # Storing all the feature names, which can be unigrams and bigrams 
     return feature_names
 
+
 # Classifying and predicting 
 def classifier_architecture():
     print("Classifying and predicting")
     classifier = MLPClassifier(activation = "logistic", # logistic = giving a value between 0-1 to the data
-                            hidden_layer_sizes = (20,), # number of neurons = 20
-                            max_iter=1000, # setting a limit of 1000
+                            hidden_layer_sizes = (20,), # one hidden layer with 20 neurons
+                            max_iter=1000, # setting a limit of 1000 iterations
                             random_state = 42) # making it reproducible
     return classifier
+
 
 def train_model(classifier, text_train_feats, label_train):
     print("Training model")
@@ -97,20 +114,20 @@ def train_model(classifier, text_train_feats, label_train):
 # Getting predictions
 def prediction(model, text_test_feats):
     print("Creating predictions")
-    y_pred = model.predict(text_test_feats) 
+    y_pred = model.predict(text_test_feats) # Generating predictions for the test data
     return y_pred
 
 # Calculating metrics 
 def classifier_report(label_test, y_pred):
-    print("Classifier Metrics:")
-    classifier_metrics = metrics.classification_report(label_test, y_pred) # Checking to see how the true labels compare with our predictions
+    print("Classifier Metrics:") # Checking to see how the true labels compared with our predictions
+    classifier_metrics = metrics.classification_report(label_test, y_pred) 
     print(classifier_metrics)
     return classifier_metrics
 
 def metrics_save_function(classifier_metrics):
     folder_path = os.path.join(".", "out")
     file_name = "neural_networks_classifier_metrics.txt"
-    file_path = os.path.join(folder_path, file_name)
+    file_path = os.path.join(folder_path, file_name) # Defining where to save the classifer report.
 
     with open(file_path, "w") as f: # "Writing" the classifier metrics, thereby saving it.
         f.write(classifier_metrics)
@@ -125,20 +142,21 @@ def model_save_function(classifier, vectorizer):
 
 def main_function():
     print("Neural Network Script:")
-    args = input_parse()
-    data = loading_data(args)
-    X, y = assigning_data(data)
-    X_train, X_test, y_train, y_test = split(X, y)
-    vectorizer = vectorizer_function()
-    X_train_feats, X_test_feats = transform(X_train, X_test, vectorizer)
-    feature_names = feature(vectorizer)
-    classifier = classifier_architecture()
-    model = train_model(classifier, X_train_feats, y_train)
-    y_pred = prediction(model, X_test_feats)
-    classifier_metrics = classifier_report(y_test, y_pred)
-    metrics_save_function(classifier_metrics)
-    model_save_function(classifier, vectorizer)
+    args = input_parse() # COmmand line arguments
+    folder_path = unzip(args) # Unzipping function
+    data = loading_data(folder_path) # Reading data as pandas dataframe
+    X, y = assigning_data(data) # Assigning data
+    X_train, X_test, y_train, y_test = split(X, y) # Splitting data
+    vectorizer = vectorizer_function() # Parameters for vectorizing
+    X_train_feats, X_test_feats = transform(X_train, X_test, vectorizer) # Transforming the data 
+    feature_names = feature(vectorizer) # Getting features
+    classifier = classifier_architecture() # The model architecture
+    model = train_model(classifier, X_train_feats, y_train) # Training the model
+    y_pred = prediction(model, X_test_feats) # Getting predictions
+    classifier_metrics = classifier_report(y_test, y_pred) # Classification report
+    metrics_save_function(classifier_metrics) # Saving report
+    model_save_function(classifier, vectorizer) # Saving model
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": # If this script is called from the terminal run main function
     main_function()
