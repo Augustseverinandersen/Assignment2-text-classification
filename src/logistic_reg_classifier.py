01a4566fd1a5290ef0b4995906488deb786da53c
Augustseverinandersen <112094086+Augustseverinandersen@users.noreply.github.com>
2023-05-24 16:18:08 +0200

    Update logistic_reg_classifier.py

--- src/logistic_reg_classifier.py
+++ src/logistic_reg_classifier.py
@@ -89,7 +89,7 @@ def transform(text_train, text_test, vectorizer):
     return X_train_feats, X_test_feats
 
 
-# get feature names
+# get feature names # Delelte removeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 def feature(vectorizer):
     feature_names = vectorizer.get_feature_names_out() # Storing all the feature names, which can be unigrams and bigrams 
     return feature_names
